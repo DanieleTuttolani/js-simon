067abab7a80971078bfcb50c4d866845ba2bd626
DanieleTuttolani <danieletuttolani2@gmail.com>
2022-12-20 20:10:11 +0100

    seconds

--- scripts/script.js
+++ scripts/script.js
@@ -1,12 +1,29 @@
+const days = document.getElementById("days")
+const hours = document.getElementById("hours")
+const minutes = document.getElementById("minutes")
+const seconds = document.getElementById("seconds")
 
-let Xmas = new Date("2022-12-25 00:00:000")
-
-let today = new Date()
 
 
-const diffTime = Math.abs( Xmas  - today );
-const diffDays = Math.floor(diffTime / (1000 * 60 * 60 *24))
 
-let secondi = today.getSeconds()
-console.log( diffTime + "tempo")
-console.log( diffDays + "giorni" , secondi)
+let prova = setInterval(()=>{
+    let today = new Date()
+    let Xmas = new Date("2022-12-25 00:00:000")
+    const diffTime = Math.abs( Xmas  - today );
+    let diffSeconds = Math.floor((diffTime % (1000 * 60)) / 1000);   
+    
+    seconds.innerText=diffSeconds
+    console.log(diffSeconds)
+}, 1000)
+
+
+
+
+
+
+
+
+//stampo i giorni 
+// days.innerText= diffDays;
+// hours.innerText= diffHours;
+// minutes.innerText= diffDays;
